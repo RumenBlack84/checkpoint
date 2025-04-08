7efb6d3f05b1f9be123e24fdec70e444219d18ec
RumenBlack84 <brian@grant.onl>
2025-04-08 17:16:15 -0400

    working on update status script and adding help to foreachcma script

---
+++ Bash-Functions/Check-Update-Status.sh
@@ -0,0 +1,64 @@
+# Forked from https://github.com/billygr/CheckPoint/blob/master/sk83520.sh
+# In order to better maintain and implement
+# sk83520 connectivity check
+# Checkmates URL where this originally appeared
+# https://community.checkpoint.com/t5/Security-Gateways/sk83520-how-to-check-connectivity-to-CP/m-p/31874/highlight/true#M2558
+# Run on the gw/mgmt curl_cli -k  https://raw.githubusercontent.com/RumenBlack84/checkpoint/refs/heads/main/sk83520.sh > sk83520.sh
+# chmod +x sk83520.sh
+# ./sk83520.sh
+# shellcheck source=/dev/null
+Check-Update-Status() {
+  source "$CPDIR/tmp/.CPprofile.sh"
+  local ProxyUrl="$1"
+
+  if [[ -z "$ProxyUrl" ]]; then
+    status_code=$(curl_cli -k --head "$1" 2>/dev/null | grep HTTP | awk '{print $2}')
+  else
+    status_code=$(curl_cli -k --proxy "$ProxyUrl" --head "$1" 2>/dev/null | grep HTTP | awk '{print $2}')
+  fi
+
+  check_url() {
+    result=" [ ERROR ]"
+    name="$2 "
+    while [ ${#name} -lt 74 ]; do name="$name."; done
+    echo -en "$name "
+    status_code=$(curl_cli -k --head "$1" 2>/dev/null | grep HTTP | awk '{print $2}')
+    case "$status_code" in
+    # Basically if it connects in anyway I'm happy, a lot of these require api calls and
+    # formatting to get proper 200 codes
+    1?? | 2?? | 3?? | 400 | 403 | 405 | 504)
+      result="[ OK ]"
+      ;;
+    *)
+      result="[ FAIL ] - Got HTTP ${status_code}"
+      ;;
+    esac
+    echo "${result}"
+  }
+
+  echo "##############################################################################################################"
+  echo "sk83520 How to verify that Security Gateway and/or Security Management Server can access Check Point servers"
+  echo "##############################################################################################################"
+  check_url 'http://cws.checkpoint.com/APPI/SystemStatus/type/short' 'Social Media Widget Detection'
+  check_url 'http://cws.checkpoint.com/URLF/SystemStatus/type/short' 'URL Filtering Cloud Categorization'
+  check_url 'http://cws.checkpoint.com/AntiVirus/SystemStatus/type/short' 'Virus Detection'
+  check_url 'http://cws.checkpoint.com/Malware/SystemStatus/type/short' 'Bot Detection'
+  check_url 'https://updates.checkpoint.com/WebService/Monitor' 'IPS Updates'
+  check_url 'http://crl.godaddy.com/gdroot.crl' 'CRL check godaddy Update Service used for revocation'
+  check_url 'http://crl.globalsign.net/root.crl' 'CRL check globalsign CRL that updates service certificate uses'
+  check_url 'http://dl3.checkpoint.com' 'Download Service Updates'
+  check_url 'https://usercenter.checkpoint.com/usercenter/services/ProductCoverageService' 'Contract Entitlement'
+  check_url 'https://usercenter.checkpoint.com/usercenter/services/BladesManagerService' 'Software Blades Manager Service'
+  check_url 'http://resolver1.chkp.ctmail.com' 'Suspicious Mail Outbreaks'
+  check_url 'http://download.ctmail.com' 'Anti-Spam'
+  check_url 'https://te.checkpoint.com/tecloud/Ping' 'Threat Emulation'
+  check_url 'http://avupdates.checkpoint.com/UrlList.txt' 'Traditional Anti-Virus, Legacy URL Filtering'
+  check_url 'http://sigcheck.checkpoint.com/Siglist2.txt' 'Download of signature updates for Traditional Anti-Virus, etc'
+  check_url 'http://secureupdates.checkpoint.com/IP-list/100k.txt' 'Manage Security Gateways'
+  check_url 'https://sc1.checkpoint.com/sc/images/checkmark.gif' 'Download of icons and screenshots from Check Point media storage servers'
+  check_url 'https://sc1.checkpoint.com/za/images/facetime/large_png/60342479_lrg.png' 'Download of icons and screenshots from Check Point media storage servers'
+  check_url 'https://sc1.checkpoint.com/za/images/facetime/large_png/60096017_lrg.png' 'Download of icons and screenshots from Check Point media storage servers'
+  check_url 'https://push.checkpoint.com/push/ping' 'Push Notifications'
+  check_url 'http://downloads.checkpoint.com' 'Download of Endpoint Compliance Updates'
+  check_url 'http://productservices.checkpoint.com' 'Entitlement/Licensing Updates'
+}
--- Bash-Functions/ForEachCma.sh
+++ Bash-Functions/ForEachCma.sh
@@ -3,6 +3,19 @@ ForEachCma() {
   # shellcheck source=/dev/null
   source "$CPDIR/tmp/.CPprofile.sh"
   local param1="$1"
+  if [[ "$1" == "--help" || "$1" == "-h" || -z "$1" ]]; then
+    echo "Usage: ForEachCma <CommandToRun>"
+    echo
+    echo "Runs your specified command against all CMAs on the MDS"
+    echo
+    echo "Example:"
+    # disalbing SC2016 for these lines as I literally want these strings
+    # shellcheck disable=SC2016
+    echo '  ForEachCMAmy_function "echo $FWDIR"'
+    # shellcheck disable=SC2016
+    echo 'This will print the $FWDIR variable on each CMA'
+    return 0
+  fi
   # run commands on all domains
   for DOMAIN in $(${MDSVERUTIL} AllCMAs); do
     echo "##############################################"
